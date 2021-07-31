6615ddffff13ddfe9d2c83823cb2ed3ee0d26e9a
JaithraVarma <75250288+JaithraVarma@users.noreply.github.com>
2021-08-01 00:37:44 +0530

    Add files via upload

--- TASK 1/snake_game/TASK 1/SpaceInvader/main.py
+++ TASK 1/snake_game/TASK 1/SpaceInvader/main.py
@@ -9,14 +9,14 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 #Title and Icon
-pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load("ufo.png")
+pygame.display.set_caption("Space Invader")
+icon = pygame.image.load("SpaceInvader\ufo.png").convert()
 pygame.display.set_icon(icon)
 
 #Background
-background = pygame.image.load("background.png")
+background = pygame.image.load("SpaceInvader\background.png")
 
-#Player
+#PlayerTASK 1
 playerImg = pygame.image.load("player.png")
 playerX = 370
 playerY = 480
